36f684cd8a9980ad76e942addcc254f22a753db8
Hasan <>
2023-12-03 17:25:20 +0300

    add debug logs

--- variables.sh
+++ variables.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+
 runner_bin="/bin/bash"
+echo "runner_bin: $runner_bin"
+
 working_dir="$BITRISE_SOURCE_DIR"
+echo "working_dir: $working_dir"
+
 script_file_path=""
+echo "script_file_path: $script_file_path"
+
 is_debug="no"
+echo "is_debug: $is_debug"
+
 SUBDIR=$(dirname "$BITRISE_PROJECT_PATH")
+echo "SUBDIR: $SUBDIR"
+
 # Build app working directory
 if [[ -n "$SUBDIR" && -d "$SUBDIR" ]]; then    
     APP_WORKING_DIR="$BITRISE_SOURCE_DIR/$SUBDIR"
 else
     APP_WORKING_DIR="$BITRISE_SOURCE_DIR"
 fi
-PROJECT_DIR=$(ls $APP_WORKING_DIR | grep ".xcodeproj$" | head -n 1)
+echo "APP_WORKING_DIR: $APP_WORKING_DIR"
+
+PROJECT_DIR=$(ls "$APP_WORKING_DIR" | grep ".xcodeproj$" | head -n 1)
+echo "PROJECT_DIR: $PROJECT_DIR"
+
 PROJECT="$APP_WORKING_DIR/$PROJECT_DIR/project.pbxproj"
+echo "PROJECT: $PROJECT"
