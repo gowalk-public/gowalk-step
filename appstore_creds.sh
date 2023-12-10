c5b2dd6f5e894453743e4fc45a32a5364fa25a24
Hasan <>
2023-12-10 13:36:45 +0300

    update checking for debug mode

--- appstore_creds.sh
+++ appstore_creds.sh
@@ -3,11 +3,11 @@
 # Check if jq is installed, if not, install it using Homebrew
 if ! command -v jq &> /dev/null
 then
-    [ "$IS_DEBUG" -eq 1 ] && echo "jq could not be found, installing it now..."
+    [ "$IS_DEBUG" -eq "1" ] && echo "jq could not be found, installing it now..."
     # Install Homebrew if it's not installed
     if ! command -v brew &> /dev/null
     then
-        [ "$IS_DEBUG" -eq 1 ] && echo "Installing Homebrew..."
+        [ "$IS_DEBUG" -eq "1" ] && echo "Installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> /dev/null
     fi
     # Install jq using Homebrew
@@ -28,6 +28,6 @@ export APPLE_KEY_ID=$key_id
 export APPLE_ISSUER_ID=$issuer_id
 export APPLE_PRIVATE_KEY="$private_key"
 
-[ "$IS_DEBUG" -eq 1 ] && echo "APPLE_KEY_ID $APPLE_KEY_ID"
-[ "$IS_DEBUG" -eq 1 ] && echo "APPLE_ISSUER_ID $APPLE_ISSUER_ID"
-[ "$IS_DEBUG" -eq 1 ] && echo "APPLE_PRIVATE_KEY $APPLE_PRIVATE_KEY"
+[ "$IS_DEBUG" -eq "1" ] && echo "APPLE_KEY_ID $APPLE_KEY_ID"
+[ "$IS_DEBUG" -eq "1" ] && echo "APPLE_ISSUER_ID $APPLE_ISSUER_ID"
+[ "$IS_DEBUG" -eq "1" ] && echo "APPLE_PRIVATE_KEY $APPLE_PRIVATE_KEY"
--- crashlytics_prepare.sh
+++ crashlytics_prepare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Create a folder named 'crashlytics' inside the application working directory
 mkdir -p "$BITRISE_SOURCE_DIR/crashlytics"
-[ "$IS_DEBUG" -eq 1 ] && echo "created dir $BITRISE_SOURCE_DIR/crashlytics"
+[ "$IS_DEBUG" -eq "1" ] && echo "created dir $BITRISE_SOURCE_DIR/crashlytics"
 
 # Copy the 'upload-symbols' file from the script's directory to the 'crashlytics' folder
 cp "${THIS_SCRIPT_DIR}/crashlytics/upload-symbols" "$BITRISE_SOURCE_DIR/crashlytics/upload-symbols"
-[ "$IS_DEBUG" -eq 1 ] && echo "$THIS_SCRIPT_DIR/crashlytics/upload-symbols copied to $BITRISE_SOURCE_DIR/crashlytics/upload-symbols"
+[ "$IS_DEBUG" -eq "1" ] && echo "$THIS_SCRIPT_DIR/crashlytics/upload-symbols copied to $BITRISE_SOURCE_DIR/crashlytics/upload-symbols"
 
 # Step 1: Find all .plist files
 plist_files=$(find $BITRISE_SOURCE_DIR -name "*.plist")
@@ -20,7 +20,7 @@ for file in $plist_files; do
             if grep -q "<key>REVERSED_CLIENT_ID</key>" "$file"; then
                 # Step 5: Copy the first matching file
                 cp "$file" "$BITRISE_SOURCE_DIR/crashlytics/GoogleService-Info.plist"
-                [ "$IS_DEBUG" -eq 1 ] && echo "$file copied to $BITRISE_SOURCE_DIR/crashlytics/GoogleService-Info.plist"
+                [ "$IS_DEBUG" -eq "1" ] && echo "$file copied to $BITRISE_SOURCE_DIR/crashlytics/GoogleService-Info.plist"
                 break
             fi
         fi
