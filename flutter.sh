ede491fd508fe6edb49161275ac618ff36a27bb4
Hasan <>
2024-02-20 21:27:48 +0300

    fix version mumber for flutter

--- flutter.sh
+++ flutter.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+if [[ $APP_VERSION =~ ^[0-9]+\.[0-9]+$ ]]; then
+    APP_VERSION="${APP_VERSION}.0"
+fi
 sed -i '' "/^version: /s/version: .*/version: $APP_VERSION+$BITRISE_BUILD_NUMBER/" $BITRISE_SOURCE_DIR/pubspec.yaml
 flutter upgrade
 flutter pub get
