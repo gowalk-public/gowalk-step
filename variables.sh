7061d7701ab53ae3c3c26fb2a397ed708a71fbca
Hasan <>
2024-02-20 20:03:21 +0300

    flutter fixes

--- variables.sh
+++ variables.sh
@@ -4,6 +4,11 @@ export runner_bin="/bin/bash"
 export working_dir="$BITRISE_SOURCE_DIR"
 export script_file_path=""
 
+if [ -d "$BITRISE_SOURCE_DIR/ios/Flutter" ]; then
+    export BITRISE_PROJECT_PATH="ios/Runner.xcodeproj"
+    export BITRISE_SCHEME="Runner"
+fi
+
 export SUBDIR=$(dirname "$BITRISE_PROJECT_PATH")
 if [ "$SUBDIR" = "." ]; then
     export SUBDIR=""
@@ -18,8 +23,3 @@ fi
 export PROJECT_DIR=$(ls "$APP_WORKING_DIR" | grep ".xcodeproj$" | head -n 1)
 export PROJECT="$APP_WORKING_DIR/$PROJECT_DIR/project.pbxproj"
 export PROJECT_FILE="$APP_WORKING_DIR/$PROJECT_DIR"
-
-if [ -d "$BITRISE_SOURCE_DIR/ios/Flutter" ]; then
-    export BITRISE_PROJECT_PATH="ios/Runner.xcodeproj"
-    export BITRISE_SCHEME="Runner"
-fi
