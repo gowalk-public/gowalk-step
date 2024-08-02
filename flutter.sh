7ce5f52209ba00830c0b401520b23b68dd530329
Hasan <>
2024-08-02 20:26:03 +0400

    update flutter script

--- flutter.sh
+++ flutter.sh
@@ -4,18 +4,4 @@ if [[ $APP_VERSION =~ ^[0-9]+\.[0-9]+$ ]]; then
 fi
 sed -i '' "/^version: /s/version: .*/version: $APP_VERSION+$BITRISE_BUILD_NUMBER/" $BITRISE_SOURCE_DIR/pubspec.yaml
 
-if [[ -z "$flutter_version" ]]; then
-    flutter upgrade
-else
-    fvm install $flutter_version
-    fvm use $flutter_version --force
-    fvm flutter --version
-    #git -C /Users/vagrant/fvm/default/bin/ checkout $flutter_version
-    export FLUTTER_ROOT="/Users/vagrant/fvm/versions/$flutter_version"
-
-fi
-
-#flutter pub get
-cd ios
 rm -rf $BITRISE_SOURCE_DIR/ios/Podfile.lock $BITRISE_SOURCE_DIR/ios/Pods/
-#pod install --repo-update
