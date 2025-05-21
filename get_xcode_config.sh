bbc27d0c2bbdf76f70916a5469fcae48cb539a12
Hasan <>
2025-05-21 19:54:37 +0300

    added logs

--- get_xcode_config.sh
+++ get_xcode_config.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
+echo "Step Get Xcode Config"
+
 if [ -n "${BITRISE_TARGET}" ]; then
     output=$(xcodebuild "-project" "$PROJECT_FILE" "-target" "$BITRISE_TARGET" "-configuration" "Release" "-showBuildSettings")
-    [ "$is_debug" = "yes" ] && echo "To get Xcode Config will be used BITRISE_TARGET"
+    echo "To get Xcode Config will be used BITRISE_TARGET"
 else
     output=$(xcodebuild "-project" "$PROJECT_FILE" "-target" "$BITRISE_SCHEME" "-configuration" "Release" "-showBuildSettings")
-    [ "$is_debug" = "yes" ] && echo "To get Xcode Config will be used BITRISE_SCHEME"
+    echo "To get Xcode Config will be used BITRISE_SCHEME"
 fi
 
+echo "Step Find the line containing PRODUCT_BUNDLE_IDENTIFIER"
 # Find the line containing PRODUCT_BUNDLE_IDENTIFIER and extract its value
 bundle_identifier=$(echo "$output" | grep -E "^ *PRODUCT_BUNDLE_IDENTIFIER =" | cut -d '=' -f2 | xargs)
 
-
+echo "Step Export the PRODUCT_BUNDLE_IDENTIFIER environment variable"
 # Export the PRODUCT_BUNDLE_IDENTIFIER environment variable
 export PRODUCT_BUNDLE_IDENTIFIER="$bundle_identifier"
 
 # Optional: Display the value (for verification)
-[ "$is_debug" = "yes" ] && echo "PRODUCT_BUNDLE_IDENTIFIER: $PRODUCT_BUNDLE_IDENTIFIER"
+echo "PRODUCT_BUNDLE_IDENTIFIER: $PRODUCT_BUNDLE_IDENTIFIER"
 
+echo "Step Find the line containing INFOPLIST_FILE and extract its value"
 # Find the line containing INFOPLIST_FILE and extract its value
 info_plist_file=$(echo "$output" | grep ' INFOPLIST_FILE =' | cut -d '=' -f2 | xargs)
 
+echo "Step Export the INFOPLIST_FILE environment variable"
 # Export the INFOPLIST_FILE environment variable
 export INFOPLIST_FILE="$APP_WORKING_DIR/$info_plist_file"
 
 # Optional: Display the INFOPLIST_FILE value (for verification)
-[ "$is_debug" = "yes" ] && echo "INFOPLIST_FILE: $INFOPLIST_FILE"
+echo "INFOPLIST_FILE: $INFOPLIST_FILE"
